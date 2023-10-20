526efdc34251211f13a1a4f26d4e8b60d1822ae1
Jose Pedro <jose.mesquita@seati.ma.gov.br>
2023-10-20 13:53:32 -0300

    update pastas e arquivos

--- PHP - POO/index.php
+++ PHP - POO/index.php
@@ -1,17 +1,63 @@
 <?php
 
-class Pessoa {
-    public string $name;
-    public string $corPele;
-    public int $idade;
-    public float $altura;
+Class Pessoa {
+    public int $id;
+    public string $name = '';
+    public string $sobrenome = '';
+    public string $carPele = '';
+    //se atributo idade for private só pode ser alterado pela class mae
+    private int $idade;
+    public float $peso;
+    //method constructor setar as paramet ao iniciar a class
+    public function __construct($PessoaId){
+        $this->id = $PessoaId;
+    }
+    public function addIdade (){
+        $this->idade++;
+    }
+
+    public function setIdade($year){
+        $this->idade = $year;
+    }
+    public function getIdade(){
+        echo 'Idade by Get '.$this->idade;
+    }
+    // quando uma function é static podesse usar ela sem precisar criar um objeto da classe, pode instanciar direta a class::function
+    public static function soma ($x, $y){
+        return $x+$y;
+    }
 }
 
+//herança class cachorro herdando atributos e metodos da classe Pessoa
+class Cachorro extends Pessoa{
+    public string $corPelo = '';
 
-$pessoa1 = new Pessoa();
-$pessoa1->name = 'José Pedro';
-$pessoa1->corPele  = 'Branca';
-$pessoa1->idade = 26;
-$pessoa1->altura = 1.85;
+    public function setCorPelo($pelo){
+        $this->corPelo = $pelo;
+    }
 
-print_r($pessoa1);
+    public function getCorPelo(){
+        return $this->corPelo;
+    }
+}
+$pessoa = new Pessoa(1);
+$pessoa->name = 'José Pedro';
+$pessoa->sobrenome = 'Mesquita';
+$pessoa->carPele = 'Branca';
+$pessoa->setIdade(25);
+$pessoa->peso = 100.3;
+
+$pessoa->setIdade(30);
+$pessoa->getIdade();
+echo "<br/><br/>";
+print_r($pessoa);
+echo "<br/><br/>";
+$pessoa->addIdade();
+print_r($pessoa);
+echo "<br/><br/>";
+echo Pessoa::soma(10,33);
+
+echo "<br/><br/>Cachorro<br/>";
+$caramelo = new Cachorro(1);
+$caramelo->setCorPelo('amarelo');
+echo $caramelo->getCorPelo();
